b70a2564c5e9ba5be3fb2994f07a8d135de375d9
888JordyEmanuelAymaCutipa888 <jayma@unsa.edu.pe>
2022-07-21 22:09:53 -0500

    Envío de variables

--- my-dream-app/src/app/app.component.ts
+++ my-dream-app/src/app/app.component.ts
@@ -5,6 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+//envío de variables usando la clase "AppComponent"
 export class AppComponent {
   title = 'my-dream-app';
+  name = 'jordy';
+  webpage = 'www.unsa.edu.pe';
+  email = 'jordy1234@gmail.com';
 }
